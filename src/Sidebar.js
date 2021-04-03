ac939921849cacb8ba9345d088056b384c805adf
ashucoding <shanawiltshire@gmail.com>
2021-04-03 15:50:14 -0400

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import './Sidebar.css';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from '@material-ui/core';
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar_top">
+                <h3>Clever Gamer</h3>
+                <ExpandMoreIcon />
+            </div>
+            
+            <div className="sidebar_channels">
+                <div className="sidebar_channelsHeader">
+                    <div className="sidebar_header">
+                    <ExpandMoreIcon />
+                    <h4>Chat Channels</h4>
+                    </div>
+                    
+                    <AddIcon className="sidebar_addChannel"/>
+                </div>
+                <div className="sidebar_channelsList">
+                 <SidebarChannel />
+                 <SidebarChannel />
+                 <SidebarChannel />
+                 <SidebarChannel />
+                </div>
+            </div>
+
+            <div className="sidebar_voice">
+                <SignalCellularAltIcon 
+                className="sidebar_voiceIcon"
+                fontSize="large"
+                />
+                <div className="sidebar_voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar_voiceIcons">
+                    <InfoOutlinedIcon />
+                    <CallIcon />
+                </div>
+            </div>
+            <div className="sidebar_profile">
+                <Avatar />
+                <div className="sidebar_profileInfo">
+                    <h3>Shana</h3>
+                    <p>#thisIsMyID</p>
+                </div>
+                <div className="sidebar_profileIcons">
+                    <MicIcon />
+                    <HeadsetIcon />
+                    <SettingsIcon />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar
---
+++ src/SidebarChannel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SidebarChannel.css';
+
+function SidebarChannel({id, channel }) {
+    return <div className="sidebarChannel">
+            <h4><span className="sidebarChannel_hash">#</span>GTA</h4>
+        </div>;
+}
+
+export default SidebarChannel
